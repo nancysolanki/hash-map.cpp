5a9a70502f8a0fc743f0f87fa17878df36ff262a
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2020-11-17 19:13:32 +0530

    Update Hash insertion of array.cpp

--- Hash insertion of array.cpp
+++ Hash insertion of array.cpp
@@ -1,1 +1,45 @@
-.
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void printItinerary(map<string, string> dataSet) 
+{ 
+	// To store reverse of given map 
+	map<string, string> reversemap; 
+	map<string, string>::iterator it; 
+
+
+	for (it = dataSet.begin(); it!=dataSet.end(); it++) 
+		reversemap[it->second] = it->first; 
+
+	// Find the starting point of itinerary 
+	string start; 
+
+	for (it = dataSet.begin(); it!=dataSet.end(); it++) 
+	{ 
+		if (reversemap.find(it->first) == reversemap.end()) 
+		{ 
+			start = it->first; 
+			break; 
+		} 
+	} 
+
+
+	if (start.empty()) 
+	{ 
+		cout << "Invalid Input" << endl; 
+		return; 
+	} 
+
+	// Once we have starting point, we simple need to go next, 
+	//next of next using given hash map 
+	it = dataSet.find(start); 
+	while (it != dataSet.end()) 
+	{ 
+		cout << it->first << "->" << it->second << endl; 
+		it = dataSet.find(it->second); 
+	} 
+
+} 
+
+contributed by Aditya Goel 
+
