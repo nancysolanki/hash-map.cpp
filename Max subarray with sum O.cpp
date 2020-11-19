0858a0e657c92fb65fe7135d8da83f2338ced87d
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2020-11-19 11:23:33 +0530

    Create Max subarray with sum O.cpp

---
+++ Max subarray with sum O.cpp
@@ -0,0 +1,68 @@
+C++ program to find the length of largest subarray 
+// with 0 sum 
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+  
+// Returns Length of the required subarray 
+
+int maxLen(int arr[], int n) 
+{ 
+
+    // Map to store the previous sums 
+
+    unordered_map<int, int> presum; 
+
+  
+
+    int sum = 0; // Initialize the sum of elements 
+
+    int max_len = 0; // Initialize result 
+
+  
+
+    // Traverse through the given array 
+
+    for (int i = 0; i < n; i++) { 
+
+        // Add current element to sum 
+
+        sum += arr[i]; 
+
+  
+
+        if (arr[i] == 0 && max_len == 0) 
+
+            max_len = 1; 
+
+        if (sum == 0) 
+
+            max_len = i + 1; 
+
+  
+
+        // Look for this sum in Hash table 
+
+        if (presum.find(sum) != presum.end()) { 
+
+            // If this sum is seen before, then update max_len 
+
+            max_len = max(max_len, i - presum[sum]); 
+
+        } 
+
+        else { 
+
+            // Else insert this sum with index in hash table 
+
+            presum[sum] = i; 
+
+        } 
+
+    } 
+
+  
+
+    return max_len; 
+} 
