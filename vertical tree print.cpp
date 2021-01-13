6fa2afa3523b64f4ee626d534fe8baa64f2845da
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-01-13 14:15:35 +0530

    Update vertical tree print.cpp

--- vertical tree print.cpp
+++ vertical tree print.cpp
@@ -1,1 +1,34 @@
+#include<unordered_map> 
+void getVerticalOrder(BinaryTreeNode<int>* root, int hd, unordered_map<int, vector<int>> &m)
+{
+  
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    m[hd].push_back(root->data);
+ 
+    
+    getVerticalOrder(root->left, hd-1, m);
+ 
+    
+    getVerticalOrder(root->right, hd+1, m);
+}
 
+void printBinaryTreeVerticalOrder(BinaryTreeNode<int>* root)
+{	 
+		
+	 unordered_map < int,vector<int> > m;
+    int hd = 0;
+    getVerticalOrder(root, hd,m);
+ 
+      unordered_map< int,vector<int> > :: iterator it;
+    for (it=m.begin(); it!=m.end(); it++)
+    {
+        for (int i=0; i<it->second.size(); ++i)
+            cout << it->second[i] << " ";
+        cout << endl;
+    }
+}
+ 
